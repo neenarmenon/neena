d6a27746259c02b7638c6f3ecb2413b5b97e7e3e
neenarmenon <neenarmenon@gmail.com>
2016-07-15 15:19:09 +0530

    ddd

---
+++ JavaProject/src/com/acc/java/strings/Palindrome.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.acc.java.strings;
+
+/**
+ * @author neena.r.menon
+ *
+ */
+public class Palindrome {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
