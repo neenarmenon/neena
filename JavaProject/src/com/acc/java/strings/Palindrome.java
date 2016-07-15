002f7cf2f4fea701dc1dc1ac582a41dac053efaf
neenarmenon <neenarmenon@gmail.com>
2016-07-15 15:40:51 +0530

    Second Commit

--- JavaProject/src/com/acc/java/strings/Palindrome.java
+++ JavaProject/src/com/acc/java/strings/Palindrome.java
@@ -14,6 +14,7 @@ public class Palindrome {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Push again to GIT remote repository");
 
 	}
 
